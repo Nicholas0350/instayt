67a1710e531cd14253d8958818a4b3a587697122
nicholas <nicholas@zainaweb.com>
2021-10-22 09:31:42 +1100

    google signin part 1 workiong

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,26 +1,8 @@
-// import NextAuth from "next-auth"
-
-// import GoogleProvider from "next-auth/providers"
-
-// export default NextAuth({
-//   // Configure one or more authentication providers
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     }),
-//     // ...add more providers here
-//   ],
-
-//   pages: {
-//     signIn: "/auth/signin",
-//   },
-// })
-
 import NextAuth from "next-auth"
+
 import GoogleProvider from "next-auth/providers/google"
 
-const na = NextAuth({
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -28,14 +10,8 @@ const na = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
-  ],   
-  debug: true,
-  // pages: {
-  //   signIn: "/auth/signin",
-  // },
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
 })
-
-export default function handler(req, res) {
-  console.log("request", req);
-  na(req, res);
-}
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,62 +1,29 @@
-// import { signIn as signIntoProvider } from "next-auth/react";
-// import { getProviders } from "next-auth/react"
+import { signIn as signIntoProvider } from "next-auth/react";
+import { getProviders } from "next-auth/react"
 
-
-// function signIn({ providers }) {
-//   return (
-//     <>
-//       {Object.values(providers).map((provider) => (
-//         <div key={provider.name}>
-//           <button onClick={() => signIntoProvider(provider.id)}>
-//             Sign in with {provider.name}
-//           </button>
-//         </div>
-//       ))}
-//     </>
-//   );
-// }
-
-// export async function getServerSideProps() {
-//   const providers = await getProviders();
-
-//   return {
-//     props: {
-//       providers,
-//     },
-//   };
-// }
-
-// export default signIn;
-
-import { getProviders, signIn as signIntoProvider } from "next-auth/react";
 
 function signIn({ providers }) {
   return (
-    <div>
-      <>
-        {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
-            <button onClick={() => signIntoProvider(provider.id)}>
-              Sign in with {provider.name}
-            </button>
-          </div>
-        ))}
-      </>
-    </div>
+    <>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => signIntoProvider(provider.id)}>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
   );
 }
 
 export async function getServerSideProps() {
-  const prov = getProviders()
-  console.log(prov);
   const providers = await getProviders();
-   console.log(providers);
+
   return {
     props: {
-      providers
-    }
-  }
+      providers,
+    },
+  };
 }
-
 
 export default signIn;
