a415a237d185bbea78af19b24887d731ed1685f0
nicholas <nicholas@zainaweb.com>
2021-10-24 10:44:10 +1100

    image uploads to firebase

--- firebase.js
+++ firebase.js
@@ -1,37 +1,11 @@
-// // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 // import { initializeApp, getApps, getApp } from "firebase/app";
 // import { getFirestore } from "firebase/firestore";
 // import { getStorage } from "firebase/storage";
-// // import { getAnalytics } from "firebase/analytics";
 
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDUL9vcNR0jZ-NHBptFT7QzuHnv937iuVM",
-//   authDomain: "nicksta-2-yt.firebaseapp.com",
-//   projectId: "nicksta-2-yt",
-//   storageBucket: "nicksta-2-yt.appspot.com",
-//   messagingSenderId: "158043330520",
-//   appId: "1:158043330520:web:b512519a19fa8a673704ad"
-//   // measurementId: "G-5SYKEL5S98"
-// };
-
-// // Initialize Firebase
-// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// const db = getFirestore();
-// const storage = getStorage();
-// // const analytics = getAnalytics(app);
-
-// export { app, db, storage };
-
-
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFireStore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -48,7 +22,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFireStore();
+const db = getFirestore();
 const storage = getStorage();
 
 export { app, db, storage };
